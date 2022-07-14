46581d8ee29c2053ff1ebd2e41f933cfbc28fc13
NicoToff <toffolo01@hotmail.com>
2022-07-14 12:30:16 +0200

    first commit

---
+++ src/Button.js
@@ -0,0 +1,11 @@
+import "./bootswatch-darkly.css";
+
+export default function SimpleButton({ text, onClickEvent }) {
+    return (
+        <button className="btn btn-lg btn-primary" onClick={onClickEvent}>
+            {text}
+        </button>
+    );
+}
+
+// info from: https://bobbyhadz.com/blog/react-open-link-in-new-tab
